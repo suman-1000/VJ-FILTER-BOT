004cfb7ea0f8fd881922f38cf32726d3aa236ff4
suman-1000 <sumangayen1000@gmail.com>
2025-01-14 22:13:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @movies_bangla_official
+# Subscribe YouTube Channel For Amazing Bot @sumangayenyt
+# Ask Doubt on telegram @movies_bangla_official
 
 
 import re
